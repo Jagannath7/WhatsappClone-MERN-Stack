f50d312892626580c1c2aa6f9164a333441f8dd2
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-12-28 15:37:23 +0530

    pusher response

--- whatsapp-frontend/src/App.js
+++ whatsapp-frontend/src/App.js
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './components/chat/Chat';
 import Sidebar from './components/sidebar/Sidebar';
-
+import Pusher from 'pusher-js'
+import pusher_config from './pusherConfig'
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher(pusher_config, {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted',  (data) => {
+    
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="App">
        <h2>Whatsapp clone</h2>
